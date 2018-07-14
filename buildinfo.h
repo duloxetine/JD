528bf5199e737f5c61206c4412ca85603b2c4925
Watanabe Keiichi <keiichi@keiblo.com>
2018-07-14 20:56:13 +0900

    add CXXFLAGS -std=c++11

--- buildinfo.h
+++ buildinfo.h
@@ -5,6 +5,7 @@
 // Build information.
 #define CC_INFO "gcc(5.4.0)"
 #define CXX_INFO "g++(5.4.0)"
+#define CONFIGURE_ARGS "'--prefix=/usr' 'CXXFLAGS=-std=c++11'"
 
 // Version information of SVN.
 #define SVN_REVISION "Unversioned directory"
